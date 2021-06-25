73b816a0b9fa7dfffda3ee26a3657f97c7b3c3ed
suzuki071 <shinee.0711.st@gmail.com>
2021-06-25 12:57:10 +0000

    [Finish] bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # before_action :authenticate_user!
-
-  # def after_sign_in_path_for(resource)
-  #   root_path # ログイン後に遷移するpathを設定
-  # end
-
   protected
 
   def configure_permitted_parameters
